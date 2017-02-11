da815272b50a60a6f6f96851dc1574bf2037f10e
Shaikat Haque <shaikathaque4@gmail.com>
2017-02-11 02:18:31 -0500

    Added functionality to Next Pun button.

--- PunGenerator/PunGenerator.Core/ViewModels/MainViewModel.cs
+++ PunGenerator/PunGenerator.Core/ViewModels/MainViewModel.cs
@@ -2,6 +2,8 @@
 using PunGenerator.Core.Models;
 using PunGenerator.Core.Services.Interfaces;
 using System.Collections.Generic;
+using System.Windows.Input;
+using System;
 
 namespace PunGenerator.Core.ViewModels
 {
@@ -9,6 +11,7 @@ namespace PunGenerator.Core.ViewModels
     {
         private IPunService _punService;
         private IRandomService _randomService;
+        private int index;
         List<Pun> puns;
 
         public MainViewModel(IPunService punService, IRandomService randomService)
@@ -17,16 +20,30 @@ namespace PunGenerator.Core.ViewModels
             _randomService = randomService;
             puns = _punService.getPunList();
             puns = _randomService.Randomize(puns);
+            index = 0;
+        }
+
+        public ICommand NextPunCommand
+        {
+            get { return new MvxCommand(() => NextPun()); }
+        }
+
+        private void NextPun()
+        {
+            index++;
+
+            //empty string tells UI to reread all properties
+            RaisePropertyChanged("");
         }
 
         public string Question
         {
-            get { return puns[0].Question; }
+            get { return puns[index].Question; }
         }
 
         public string Answer
         {
-            get { return puns[0].Answer; }
+            get { return puns[index].Answer; }
         }
 
     }
